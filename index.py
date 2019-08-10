b1be9473b154b209acbf5ef32d37787725865f57
nickdujakovich <nickdujakovich@hotmail.com>
2019-08-09 18:43:59 -0700

    added search by zipcode/city/country functionality

--- index.py
+++ index.py
@@ -1,17 +1,35 @@
 import requests
 import APIKEY
-from flask import Flask, render_template
-url = "https://api.darksky.net/forecast/{}/37.8267,-122.4233".format(APIKEY.key)
-r = requests.get(url)
-json = r.json()
+from flask import Flask, request, render_template
+
+def callAPI(latlon = [37.8267,-122.4233]):
+    url = "https://api.darksky.net/forecast/{}/{},{}".format(APIKEY.key, latlon[0], latlon[1])
+    r = requests.get(url)
+    json = r.json()
+    return json
+
+def getLatLon(zipcode):
+    url = "https://api.opencagedata.com/geocode/v1/json?q={}&key={}".format(zipcode,APIKEY.geocodeKey)
+    r = requests.get(url)
+    json = r.json()
+    lat = json["results"][0]["geometry"]["lat"]
+    lon = json["results"][0]["geometry"]["lng"]
+    return [lat,lon]
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
+    json = callAPI()
     return render_template('index.html', json = json)
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    zipcode = request.form['zipcode']
+    latlon = getLatLon(zipcode)
+    json = callAPI(latlon)
+    return render_template('index.html', json = json, zipcode = zipcode)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
     
